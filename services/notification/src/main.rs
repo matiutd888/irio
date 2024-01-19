bbe0bb8aa839e9ce18151f964a2ef593d1021ea4
Mateusz <matiutd888@gmail.com>
2024-01-19 15:13:03 +0100

    Add env logger

--- services/notification/src/main.rs
+++ services/notification/src/main.rs
@@ -4,10 +4,28 @@ mod domain;
 mod notification_sender;
 mod notification_service;
 
+use log::{info, LevelFilter};
+
 use anyhow::{Ok, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::Builder::new()
+    .filter_level(LevelFilter::Info)
+    .format(|buf, record| {
+        writeln!(
+            buf,
+            r#"{{"timestamp":"{}","level":"{}","message":"{}","module":"{}","line":{}}}"#,
+            chrono::Utc::now().to_rfc3339(),
+            record.level(),
+            record.args(),
+            record.module_path().unwrap_or_default(),
+            record.line().unwrap_or(0),
+        )
+    })
+    .init();
+
+
     let j = tokio::spawn(notification_service::run_notification_service());
     let _ = j.await;
     Ok(())
